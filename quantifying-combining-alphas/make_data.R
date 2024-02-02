7e5e7c6d6f6137ce82093a4823e5dc11c9906498
Kris Longmore <kris@robotwealth.com>
2024-02-02 20:54:25 +0800

    add recent articles

---
+++ quantifying-combining-alphas/make_data.R
@@ -0,0 +1,91 @@
+# make binance perp universe
+# exclude anything with price < 0.01 (arbitrary)
+# exclude the 20% remaining with lowest trailing monthly volume.
+
+library(rwRtools)
+rwlab_data_auth()
+
+futures <- rwRtools::crypto_get_binance_spot_1h()
+
+futures %>% 
+  summarise(
+    min_date = min(Datetime), 
+    max_date = max(Datetime)
+  )
+
+# snippet: rw crypto load ftx perpetual funding rates v0.1
+perp_rate <- rwRtools::crypto_get_binance_perps_funding()
+
+perp_rate %>%
+  summarise(
+    min_date = min(funding_time), 
+    max_date = max(funding_time)
+  )
+
+universe <- futures %>% 
+  left_join(perp_rate, by = c('Ticker','Datetime'='funding_time')) %>%
+  replace_na(list(funding_rate = 0))
+
+universe %>% head()
+
+universe <- universe %>%
+  group_by(Ticker) %>%
+  arrange(Datetime) %>%
+  mutate(
+    m2m_returns_simple = Close/lag(Close, 1) - 1,
+    m2m_returns_log = log(m2m_returns_simple + 1),
+    funding_returns_simple = lead(-funding_rate),
+    funding_returns_log = log(funding_returns_simple + 1),
+    total_returns_simple = m2m_returns_simple + funding_returns_simple,
+    total_returns_log = log(total_returns_simple + 1)
+  )
+
+universe %>% head()
+
+universe <- universe %>% 
+  mutate(
+    is_universe_price = min(Open, High, Low, Close) >= 0.01,
+    trail_volume = roll_mean(Volume, 30 * 24)
+  ) %>%
+  na.omit() %>%
+  group_by(Datetime) %>%
+  mutate(
+    volume_decile = ntile(trail_volume, 10),
+    is_universe_volume = volume_decile >= 3,
+    is_universe = is_universe_price & is_universe_volume
+  )
+
+daily <- universe %>% 
+  mutate(
+    date = as_date(Datetime)
+  ) %>%
+  group_by(date, Ticker) %>%
+  arrange(Datetime) %>% 
+  summarize(
+    open = first(Open),
+    high = max(High),
+    low = min(Low),
+    close = last(Close),
+    volume = sum(Volume),
+    m2m_returns_log = sum(m2m_returns_log),
+    m2m_returns_simple = exp(m2m_returns_log) - 1,
+    funding_returns_log = sum(funding_returns_log),
+    funding_returns_simple = exp(funding_returns_log) - 1,
+    total_returns_simple = m2m_returns_simple + funding_returns_simple,
+    total_returns_log = log(total_returns_simple + 1),
+    is_universe = sum(is_universe) > 0
+  )
+
+daily %>% head()
+
+# filter on first perp date
+daily <- daily %>%
+  filter(date > "2019-09-10") %>%
+  rename(ticker = "Ticker")
+
+head(daily)
+
+daily %>% 
+  select(-m2m_returns_log, -m2m_returns_simple, -funding_returns_simple, -total_returns_simple) %>% 
+  write_csv("binance_perp_daily.csv")
+
